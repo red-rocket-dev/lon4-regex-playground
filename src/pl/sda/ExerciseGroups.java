452741e63124b7f34994f05686192c5ba11b5a85
Jakub <kubplon@gmail.com>
2020-05-17 12:00:00 +0200

    some group exercises completed

--- src/pl/sda/ExerciseGroups.java
+++ src/pl/sda/ExerciseGroups.java
@@ -1,6 +1,8 @@
 package pl.sda;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExerciseGroups {
     private final static String METEO_STRING = "Poniedzialek;15C wiatr wschod mocny\n" +
@@ -25,7 +27,7 @@ public class ExerciseGroups {
     public static void main(String[] args) {
         double averageTemperature = averageTemperature();
         System.out.println("AVG temp: " + averageTemperature);
-        if (averageTemperature == 16.25) {
+        if (averageTemperature == 13.75) {
             System.out.println("Zadanie 1 zrobione");
         }
 
@@ -70,17 +72,39 @@ public class ExerciseGroups {
 
         String[] taxiOrder = taxiOrder();
         String[] expectedTaxiOrder = {"Zamowienie na Dworzec", "Zamowienie na Galerie", "Zamowienie na Sokolskiej", "Zamowienie na Supersamie"};
-        if(Arrays.equals(taxiOrder, expectedTaxiOrder)) {
+        if (Arrays.equals(taxiOrder, expectedTaxiOrder)) {
             System.out.println("Zadanie 5 zrobione");
         }
     }
 
-    //1. simpleString przetwórz tak, aby wyliczyć średnią temperaturę
+    /*
+    private final static String METEO_STRING = "Poniedzialek;15C wiatr wschod mocny\n" +
+            "Wtorek}16C wiatr zachod mocny\n" +
+            "Sroda[15C wiatr brak brak\n" +
+            "Czwartek!9C wiatr polnoc sredni\n";
+     */
+    //1. METEO_STRING przetwórz tak, aby wyliczyć średnią temperaturę
     public static double averageTemperature() {
-        return 0;//ta linijka jest tylko po to, żeby nie podkreslało
+        System.out.println(METEO_STRING);
+        Pattern temperaturePattern = Pattern.compile("(\\d+)C");
+        Matcher temperatureMatcher = temperaturePattern.matcher(METEO_STRING);
+
+        float sum = 0;
+        int findCounter = 0;
+        while (temperatureMatcher.find()) {
+            String temperatureText = temperatureMatcher.group(1);
+            int temperature = Integer.parseInt(temperatureText);
+            sum = sum + temperature;
+            findCounter++;
+        }
+        float average = sum / findCounter;
+        System.out.println(average);
+
+
+        return average;//ta linijka jest tylko po to, żeby nie podkreslało
     }
 
-    //2. simpleString przetwórz tak, aby wyświetlić dla każdego rekordu "W <dzień tygodnia> wiało <siła wiatru> z <kierunek>"
+    //2. TAXI_STRING przetwórz tak, aby wyświetlić dla każdego rekordu "W <dzień tygodnia> wiało <siła wiatru> z <kierunek>"
     public static String[] meteoHistory() {
         return new String[0];
     }
@@ -95,8 +119,50 @@ public class ExerciseGroups {
         return "";//ta linijka jest tylko po to, żeby nie podkreslało
     }
 
+
+    /*
+    private final static String[] TAXI_STRINGS = {"Poprosze taksowke na Dworzec" -> Zamowienie na Dworzec
+            "Taksowka pod Galerie szybko", -> Zamowienie na Galerie
+            "Gdzie ta taksowka?Miala byc 30 minut temu na Sokolskiej", -> Zamowienie na Sokolskiej
+            "Platnosc karta za 3 minuty w Supersamie dziekuje"}; -> Zamowienie na Supersamie
+     */
     //5. TAXI_STRINGS przetwórz tak, żeby wypisać dla każdego elementu listy wiadomość według szablonu: „Zamowienie na <gdzie klient zamawial taksowke>”
-    public static String[] taxiOrder() {
+    public static String[] taxiOrder1() {
+        // tworzymy tablice wynikowa Stringow, ktora bedzie przechowywala rezultat naszej operacji. Dlugosc rowna dlugosci TAXI_STRINGS
+        // for each po wszystkich elementach TAXI STRINGS
+        //     uzyc replaceAll na obecnie iterowanym elemencie, tak, zeby zamienic ciag znakow na Zamowienie na <miejsce>
+        //     na pierwszy wolny indeks w tablicy wynikowej ustawiamy rezultat operacji replaceAll
+        //     zeby miec pierwszy wolny indeks trzeba uzyc countera, zeby wiedziec ile pozycji juz zostalo zapelnionych
+        //zwracamy tablice wynikowa
+        //"Gdzie ta taksowka?Miala byc 30 minut temu na Sokolskiej"
+        System.out.println(TAXI_STRINGS[2].replaceAll(".*([A-Z a-z]+ (na)?(pod)?w?) ([A-Za-z]+).*", "Zamowienie na $4"));
+
+
         return new String[0];//ta linijka jest tylko po to, żeby nie podkreslało
+    }
+
+    public static String[] taxiOrder() {
+        String regex = ".*([A-Z a-z]+ (na)?(pod)?w?) ([A-Za-z]+).*";
+        int counter = 0;
+        String arrayFinal[] = new String[TAXI_STRINGS.length];
+        for (String element : TAXI_STRINGS) {
+            arrayFinal[counter] = element.replaceAll(regex, "Zamowienie na $4");
+            counter++;
+        }
+        return arrayFinal;//ta linijka jest tylko po to, żeby nie podkreslało }
+    }
+
+    public static String[] taxiOrderAlicja() {
+        String[] nowaTablice = new String[TAXI_STRINGS.length];
+        String regex = ".*([A-Za-z]+ (na)?(pod)?w?) ([A-Za-z]+).*";
+        int counter = 0;
+        String wordForArray = "";
+        for (String word : TAXI_STRINGS) {
+            wordForArray = word.replaceAll(regex, "Zamowienie na $4");
+            nowaTablice[counter] = wordForArray;
+            counter++;
+        }
+        System.out.println(Arrays.toString(nowaTablice));
+        return nowaTablice;
     }
 }
--- src/pl/sda/MainGroups.java
+++ src/pl/sda/MainGroups.java
@@ -1,14 +1,59 @@
 package pl.sda;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainGroups {
 
     public static void main(String[] args) {
-        //1. Chcę rozbić d czynniki pierwsze - część przed małpą, część po małpie i kraj (po ostatniej kropce)
+        //TODO: Patterns
+
+
+        //1. Chcę rozbić czynniki pierwsze - część przed małpą, część po małpie i kraj (po ostatniej kropce)
         String emailAddress1 = "test@gmail.com";
-        String emailAddress2 = "jan@interia.wp.pl";
+        String emailAddress2 = "jan@interia.pl";
+        String emailAddress3 = "jakubplonka@wp.pl";
+        String aLotOfMails = new StringBuilder(emailAddress1)
+                .append("\n")
+                .append(emailAddress2)
+                .append("\n")
+                .append(emailAddress3)
+                .toString();
+        String mailRegex = "([A-Za-z]+)@[A-Za-z]+\\.([A-Za-z]+)";
+        //System.out.println(emailAddress1.replaceAll("([A-Za-z]+)@[A-Za-z]+\\.([A-Za-z]+)", "$1@onet.$2"));
+
+        //System.out.println(aLotOfMails);
+        Pattern mailPattern = Pattern.compile(mailRegex);
+        Matcher mailMatcher = mailPattern.matcher(aLotOfMails);
+        while (mailMatcher.find()) {
+            //System.out.println("Znalazlem!");
+            //System.out.println(mailMatcher.group(1));
+            //System.out.println(mailMatcher.group(2));
+        }
+
+
+        //System.out.println(emailAddress1.matches(mailRegex));
+        //System.out.println(emailAddress3.replaceAll("([A-Za-z]+)@[A-Za-z]+(\\.[A-Za-z]+)", "$1@onet?$2"));
+        //emailAddress1.replaceAll(mailRegex,"");
+
+//System.out.println(emailAddress1.replaceAll("[A-Za-z]+@[A-Za-z]+\\.[A-Za-z]+","$1@onet.$2"));
         //2. Chcę sformatować numer telefonu do postaci 123-123-123
-        String phoneNumber = "123-41 2 322";
+        String phoneNumber = "9 2 3-4 1 2 3 2 2";
+        //1-2 3 1-2 3 1 2 3
+        //System.out.println(phoneNumber.matches("\\d[ -]?\\d[ -]?\\d[ -]?\\d[ -]?\\d[ -]?\\d[ -]?\\d[ -]?\\d[ -]?\\d[ -]?"));
+
+        //2. Chcę sformatować numer telefonu do postaci 123-123-123
+        System.out.println(phoneNumber.replaceAll("[ -]", " "));
+        String phoneNumber1 = phoneNumber.replaceAll("(\\d \\d \\d ) ", "$1-");
+        System.out.println(phoneNumber1);
+        System.out.println(phoneNumber1.replaceAll(" ", ""));
+
+
         //3. Ze zdania chcę wyciągnąć drugie słowo, ale może składać się tylko z liter
-        String someSentence = "To jest jakies zdanie";
+        String someSentence1 = "To jest jakies zdanie";
+        String someSentence2 = "Test Kowalski";
+        String someSentence3 = "Jan 123";
+        //System.out.println(someSentence1.replaceAll("\\w+ (\\w+).*", "$1"));
+        //System.out.println(someSentence1.replaceAll("To jest", "jest"));
     }
 }
